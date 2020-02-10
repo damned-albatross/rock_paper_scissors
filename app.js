bcc1f2f8f5a853d303bdde55534e40bbbb2da463
Amber <agordon5414@gmail.com>
2020-02-10 15:49:59 -0500

    created event listeners for each  button

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 const userScore =0;
 const computerScore=0;
-//the below are DOM variables - not the same as normal variables because they are in a Span tag
+//the below are DOM variables - not the same as normal variables because they are referencing tags in our HTML
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreboard_div = document.querySelector(".scoreboard");
@@ -11,6 +11,16 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+rock_div.addEventListener('click', function() {
+    console.log("hey you clicked on rock")
+})
+paper_div.addEventListener('click', function() {
+    console.log("hey you clicked on paper")
+})
+scissors_div.addEventListener('click', function() {
+    console.log("hey you clicked on scissors")
+})
 
 //function for accordian
 for (i = 0; i < acc.length; i++) {
