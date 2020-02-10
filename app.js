f5d66905db98f5f74f4c637cc0f5c5d88840bbfa
Amber <agordon5414@gmail.com>
2020-02-10 15:43:50 -0500

    cached the DOM

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+// below are my gloabal variables
 var acc = document.getElementsByClassName("accordion");
 var i;
+const userScore =0;
+const computerScore=0;
+//the below are DOM variables - not the same as normal variables because they are in a Span tag
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreboard_div = document.querySelector(".scoreboard");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
+//function for accordian
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
