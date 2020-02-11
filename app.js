7c815efabd5e472aecc80d951cd23f311a51feef
Amber <agordon5414@gmail.com>
2020-02-10 19:17:41 -0500

    tested - reset button

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // below are my gloabal variables
 var acc = document.getElementsByClassName("accordion");
+var res = document.getElementsByClassName("reset-game");
 var i;
 let userScore =0;
 let computerScore=0;
+let newGame = 0;
 //the below are DOM variables - not the same as normal variables because they are referencing tags in our HTML
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
@@ -12,6 +14,7 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+//function declares randomized computerChoice
 function getComputerChoice() {
     const choices= ["r", "p", "s"];
     const randomNumber = (Math.floor(Math.random()*3));
@@ -19,10 +22,9 @@ function getComputerChoice() {
     return choices [randomNumber];
 }
 
+//function that compares userChoice and computerChoice to declare winner
 function game(userChoice) {
     const computerChoice = getComputerChoice();
-    console.log("user choice ==>" + userChoice);
-    console.log("computer choice ==>" + computerChoice);
     switch (userChoice + computerChoice) {
         //win statements
         case "pr":
@@ -46,31 +48,36 @@ function game(userChoice) {
 }
 game();
 
+//function to convert userChoice to word
 function convertToWord (letter) {
     if (letter === "r") return "Rock";
     if (letter === "p") return "Paper";
     return "Scissors"
 }
 
+//function that runs when a user win occurs
 function win(userChoice, computerChoice) {
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore
-    result_p.innerHTML = `Your ${convertToWord(userChoice)} beats Computer's ${convertToWord(computerChoice)} </br> You Win!`;
+    result_p.innerHTML = `Your ${convertToWord(userChoice)} beats Computer's ${convertToWord(computerChoice)} </br> You Win the Round!`;
 }
 
+//function that runs when a user loss occurs
 function lose(userChoice, computerChoice) {
     computerScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore
-    result_p.innerHTML = `Computer's ${convertToWord (computerChoice)} beats Your ${convertToWord(userChoice)} </br> You Lose!`;
+    result_p.innerHTML = `Computer's ${convertToWord (computerChoice)} beats Your ${convertToWord(userChoice)} </br> You Lose the Round!`;
     
 }
 
+//function that runs when a draw occurs
 function draw(userChoice, computerChoice) {
     result_p.innerHTML = `Your ${convertToWord (userChoice)} is the same as Computer's ${convertToWord(computerChoice)} </br> It's a Tie!`;
 }
 
+//function for capturing customer choice
 function main() {
     rock_div.addEventListener('click', function() {
         game("r")
@@ -82,10 +89,8 @@ function main() {
         game("s")
     })  
 }
-
 main();
-
-//function for accordian
+//Loop when engaging with accordian
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
@@ -97,3 +102,10 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+//function for reset button
+function reset () {
+    document.getElementById("user-score").innerHTML = 0
+    document.getElementById("computer-score").innerHTML = 0
+}
+reset ();
